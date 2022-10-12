d8bffac32d2e71bfe459ff1ef1a6579036444ad7
Vlad <shugaevvladislav@iclod.com>
2022-10-12 20:44:46 +0300

    first

--- Task2.js
+++
@@ -1,8 +0,0 @@
-let styles = (a) => {
-  a.push("Рок-н-ролл");
-  a[Math.floor(a.length / 2)] = "Классика";
-  a.shift();
-  a.unshift("Рэп", "Регги");
-  return a;
-};
-console.log(styles(["Джаз", "Блюз"]));
